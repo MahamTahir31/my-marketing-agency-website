f39928389affe24d1123bf489708d50950a3a0d1
Maham Tahir <mahamst31@gmail.com>
2026-07-27 12:40:18 +0500

    Remove static export config for full-stack hosting

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export', // Enables static export
-  images: {
-    unoptimized: true, // Required because Next.js default image optimization relies on a server
-  },
+const nextConfig: NextConfig = {
+  devIndicators: false,
 };
+
 export default nextConfig;
